4c5e9f5e4f3216ad889ade31af002024e6ff83b1
James Banh <jbanh@ten-x.com>
2018-04-27 10:40:55 -0700

    finishing push for login and search with filters test

--- features/redfin_login.feature
+++ features/redfin_login.feature
@@ -1,11 +1,11 @@
 Feature: Login to Redfin and Search
   I want to log into Redfin.com with a registered account
 
-#  Scenario: Login
-#    Given I have navigated to Redfin
-#    When I click the login button
-#    Then I can use my credentials to login
-#    Then I will be logged in
+  Scenario: Login
+    Given I have navigated to Redfin
+    When I click the login button
+    Then I can use my credentials to login
+    Then I will be logged in
 
   Scenario: Login and Search
     Given I have logged into Redfin
--- features/step_definitions/redfin_steps.rb
+++ features/step_definitions/redfin_steps.rb
@@ -23,12 +23,12 @@ end
 
 Given (/^I have logged into Redfin$/) do
   visit Capybara.app_host
-  # click_button('Log In')
-  # click_button('Continue with Email')
-  # expect(page).to have_button('Sign In')
-  # fill_in('Email', :with => 'jbanh12@yahoo.com')
-  # fill_in('Password', :with => 'Acorns!')
-  # click_button('Sign In')
+  click_button('Log In')
+  click_button('Continue with Email')
+  expect(page).to have_button('Sign In')
+  fill_in('Email', :with => 'jbanh12@yahoo.com')
+  fill_in('Password', :with => 'Acorns!')
+  click_button('Sign In')
 end
 
 When (/^I make a search$/) do
@@ -45,7 +45,23 @@ And (/^Apply a filter for bed, bath, and sqft$/) do
   first('[data-rf-test-name="Stepper"]').click
   page.find(:xpath, '//div[@id=\'filterContent\']/div/div/div/div[2]/div[2]/span/span/span[2]/div/div/div/div[3]').click
   first(:xpath, '//div[@id=\'filterContent\']/div/div[2]/div/div/div[2]/div/span/span/span/span/span').click
-  page.find(:xpath, '//div[@id=\'filterContent\']/div/div[2]/div/div/div[2]/div/span/span/span/div/div/div/div[4]/span').click
+  page.find(:xpath, '//div[@id=\'filterContent\']/div/div[2]/div/div/div[2]/div/span/span/span/div/div/div/div[5]/span').click
   page.find(:xpath, '//div[@id=\'searchForm\']/form/div[2]/div/div/button/span').click
   expect(page).to have_selector('[data-rf-test-id="filterButton"]')
+end
+
+Then (/^I will receive results related to my filter$/) do
+  click_button('Table')
+  page.all(:css, '.tableRow').each do |x|
+    Integer(x.find(:css, '.column_4').text).should be >= 2
+  end
+
+  page.all(:css, '.tableRow').each do |x|
+    Float(x.find(:css, '.column_5').text).should be >= 1.25
+  end
+
+  page.all(:css, '.tableRow').each do |x|
+    puts x.find(:css, '.column_6').text
+    Integer((x.find(:css, '.column_6').text).gsub(',', '')).should be >= 1250
+  end
 end
