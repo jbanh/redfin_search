44c1be14b16b033acb640bfab0192b78c246375a
James Banh <jbanh@ten-x.com>
2018-04-27 23:42:21 -0700

    implementing page object pattern

--- features/pages/login.rb
+++ features/pages/login.rb
@@ -1,5 +1,115 @@
-class Login
-  def click_login
-    click_button('Log In')
+module Pages
+  module LoginModal
+    class << self
+      def login_button
+        return 'Log In'
+      end
+
+      def continue_email_button
+        return 'Continue with Email'
+      end
+
+      def sign_in_button
+        return 'Sign In'
+      end
+
+      def email_field
+        return 'Email'
+      end
+
+      def password_field
+        return 'Password'
+      end
+
+      def user_menu
+        return '[data-rf-test-name="UserMenu"]'
+      end
+    end
+  end
+
+  module SearchFilter
+    class << self
+      def search_bar
+        return 'City, Address, School, Agent, ZIP'
+      end
+
+      def first_search_selection
+        return '[data-rf-test-name="item-row-active"]'
+      end
+
+      def filter_button
+        return '[data-rf-test-id="filterButton"]'
+      end
+
+      def filter_link
+        return 'Filters'
+      end
+
+      def apply_filter
+        return '[data-rf-test-id="apply-search-options"]'
+      end
+
+      def min_bed_dropdown
+        return '//div[@id=\'filterContent\']/div/div/div/div/div[2]/div/span/span/span/span/span'
+      end
+
+      def min_bed_select_two
+        return '//div[@id=\'filterContent\']/div/div/div/div/div[2]/div/span/span/span/div/div/div/div[4]'
+      end
+
+      def min_bath_dropdown
+        return '[data-rf-test-name="Stepper"]'
+      end
+
+      def min_bath_select_one_quarter
+        return '//div[@id=\'filterContent\']/div/div/div/div[2]/div[2]/span/span/span[2]/div/div/div/div[3]'
+      end
+
+      def square_footage_dropdown
+        return '//div[@id=\'filterContent\']/div/div[2]/div/div/div[2]/div/span/span/span/span/span'
+      end
+
+      def sqft_select_1250
+        return '//div[@id=\'filterContent\']/div/div[2]/div/div/div[2]/div/span/span/span/div/div/div/div[5]/span'
+      end
+
+      def apply_search_button
+        return '//div[@id=\'searchForm\']/form/div[2]/div/div/button/span'
+      end
+    end
+  end
+
+  module ResultTable
+    class << self
+      def table_view
+        return 'Table'
+      end
+
+      def bedroom_column
+        return '.column_4'
+      end
+
+      def bathroom_column
+        return '.column_5'
+      end
+
+      def sqft_column
+        return '.column_6'
+      end
+    end
+  end
+end
+
+module Kernel
+  def login_screen
+    Pages::LoginModal
+  end
+
+  def search_filter
+    Pages::SearchFilter
+  end
+
+  def results_table
+    Pages::ResultTable
   end
 end
--- features/step_definitions/redfin_steps.rb
+++ features/step_definitions/redfin_steps.rb
@@ -1,67 +1,57 @@
 # frozen_string_literal: true
+require_relative '../pages/login'
 
 Given (/^I have navigated to Redfin$/) do
   visit Capybara.app_host
 end
 
 When (/^I click the login button$/) do
-  click_button('Log In')
-  expect(page).to have_button('Continue with Email')
+  click_button(login_screen.login_button)
+  expect(page).to have_button(login_screen.continue_email_button)
 end
 
 Then (/^I can use my credentials to login$/) do
-  click_button('Continue with Email')
-  expect(page).to have_button('Sign In')
-  fill_in('Email', :with => 'jbanh12@yahoo.com')
-  fill_in('Password', :with => 'Acorns!')
-  click_button('Sign In')
+  click_button(login_screen.continue_email_button)
+  expect(page).to have_button(login_screen.sign_in_button)
+  fill_in(login_screen.email_field, :with => 'jbanh12@yahoo.com')
+  fill_in(login_screen.password_field, :with => 'Acorns!')
+  click_button(login_screen.sign_in_button)
 end
 
 Then (/^I will be logged in$/) do
-  expect(page).to have_selector('[data-rf-test-name="UserMenu"]')
-end
-
-Given (/^I have logged into Redfin$/) do
-  visit Capybara.app_host
-  click_button('Log In')
-  click_button('Continue with Email')
-  expect(page).to have_button('Sign In')
-  fill_in('Email', :with => 'jbanh12@yahoo.com')
-  fill_in('Password', :with => 'Acorns!')
-  click_button('Sign In')
+  expect(page).to have_selector(login_screen.user_menu)
 end
 
 When (/^I make a search$/) do
-  fill_in('City, Address, School, Agent, ZIP', :with => 'Newport Beach')
-  first('[data-rf-test-name="item-row-active"]').click
-  expect(page).to have_selector('[data-rf-test-id="filterButton"]')
+  fill_in(search_filter.search_bar, :with => 'Newport Beach')
+  first(search_filter.first_search_selection).click
+  expect(page).to have_selector(search_filter.filter_button)
 end
 
 And (/^Apply a filter for bed, bath, and sqft$/) do
-  click_button('Filters')
-  expect(page).to have_selector('[data-rf-test-id="apply-search-options"]')
-  first(:xpath, '//div[@id=\'filterContent\']/div/div/div/div/div[2]/div/span/span/span/span/span').click
-  page.find(:xpath, '//div[@id=\'filterContent\']/div/div/div/div/div[2]/div/span/span/span/div/div/div/div[4]').click
-  first('[data-rf-test-name="Stepper"]').click
-  page.find(:xpath, '//div[@id=\'filterContent\']/div/div/div/div[2]/div[2]/span/span/span[2]/div/div/div/div[3]').click
-  first(:xpath, '//div[@id=\'filterContent\']/div/div[2]/div/div/div[2]/div/span/span/span/span/span').click
-  page.find(:xpath, '//div[@id=\'filterContent\']/div/div[2]/div/div/div[2]/div/span/span/span/div/div/div/div[5]/span').click
-  page.find(:xpath, '//div[@id=\'searchForm\']/form/div[2]/div/div/button/span').click
-  expect(page).to have_selector('[data-rf-test-id="filterButton"]')
+  click_button(search_filter.filter_link)
+  expect(page).to have_selector(search_filter.apply_filter)
+  first(:xpath, search_filter.min_bed_dropdown).click
+  page.find(:xpath, search_filter.min_bed_select_two).click
+  first(search_filter.min_bath_dropdown).click
+  page.find(:xpath, search_filter.min_bath_select_one_quarter).click
+  first(:xpath, search_filter.square_footage_dropdown).click
+  page.find(:xpath, search_filter.sqft_select_1250).click
+  page.find(:xpath, search_filter.apply_search_button).click
+  expect(page).to have_selector(search_filter.filter_button)
 end
 
 Then (/^I will receive results related to my filter$/) do
-  click_button('Table')
+  click_button(results_table.table_view)
   page.all(:css, '.tableRow').each do |x|
-    Integer(x.find(:css, '.column_4').text).should be >= 2
+    Integer(x.find(:css, results_table.bedroom_column).text).should be >= 2
   end
 
   page.all(:css, '.tableRow').each do |x|
-    Float(x.find(:css, '.column_5').text).should be >= 1.25
+    Float(x.find(:css, results_table.bathroom_column).text).should be >= 1.25
   end
 
   page.all(:css, '.tableRow').each do |x|
-    puts x.find(:css, '.column_6').text
-    Integer((x.find(:css, '.column_6').text).gsub(',', '')).should be >= 1250
+    Integer((x.find(:css, results_table.sqft_column).text).gsub(',', '')).should be >= 1250
   end
 end
